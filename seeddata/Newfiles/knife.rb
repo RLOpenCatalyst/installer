adf0304104f8bcc3568d6b567502543e88ba4556
Relevance Lab <it@relevancelab.com>
2016-03-21 17:59:52 +0530

    Update knife.rb

--- seeddata/Newfiles/knife.rb
+++ seeddata/Newfiles/knife.rb
@@ -1,11 +1,12 @@
 # See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
 
+systemhostname=`hostname -f`.chomp
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "opensource"
-client_key               "#{current_dir}/opensource.pem"
-validation_client_name   "opensource-validator"
-validation_key           "#{current_dir}/opensource-validator.pem"
-chef_server_url          "https://chefservercat.cloudapp.net/organizations/opensource"
+node_name                "catadmin"
+client_key               "#{current_dir}/catadmin.pem"
+validation_client_name   "phoenix-validator"
+validation_key           "#{current_dir}/phoenix-validator.pem"
+chef_server_url          "https://#{systemhostname}/organizations/phoenix"
 cookbook_path            ["#{current_dir}/../cookbooks"]
