b2a518ffa6c23ae39cd6f962ebc00532b81c45b8
Manaswinee Mohanty <RLE0145@rlpl.com>
2016-04-15 16:38:27 +0530

    changes for seeddaaloader

--- seeddata/Newfiles/knife.rb
+++ seeddata/Newfiles/knife.rb
@@ -1,13 +1,13 @@
 # See https://docs.chef.io/config_rb_knife.html for more information on knife configuration options
-
+hostname1=`hostname -f`.chomp
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "pcjoshi9"
-client_key               "#{current_dir}/pcjoshi9.pem"
-validation_client_name   "cat1-validator"
-validation_key           "#{current_dir}/cat1-validator.pem"
-chef_server_url          "https://api.opscode.com/organizations/cat1"
+node_name                "catadmin"
+client_key               "#{current_dir}/catadmin.pem"
+validation_client_name   "phoenix-validator"
+validation_key           "#{current_dir}/phoenix-validator.pem"
+chef_server_url          "https://#{hostname1}/organizations/phoenix"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            ["#{current_dir}/../cookbooks"]
