134fb5527d92830b30fbfa6c07f852506ddd0756
hamid1011 <hamidraza1011@gmail.com>
2016-07-14 11:48:36 +0530

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -8,6 +8,6 @@ mongodb() {
 }
 
 mongodb
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-service mongod restart
+sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+sudo service mongod restart
 
