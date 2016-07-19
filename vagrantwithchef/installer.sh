72f17063b0091e3e2c153d743e23af78e955c2de
hamid1011 <hamidraza1011@gmail.com>
2016-07-19 09:40:17 +0530

    Update installer.sh

--- vagrantwithchef/installer.sh
+++ vagrantwithchef/installer.sh
@@ -22,33 +22,55 @@ seeddataloader() {
 catalystdeploy()
 {
     #Install the Catalyst
-    cd ~
-    sudo git clone https://github.com/RLOpenCatalyst/core.git /opt/core
-    sudo mv /opt/core /opt/rlcatalyst
-    cd /opt/rlcatalyst/server
+    cd $PWD
+    sudo git clone https://github.com/RLOpenCatalyst/core.git
+    sudo mv core rlcatalyst
+    cd rlcatalyst/client/cat3
+    sudo npm install --production
+    sudo npm run-script build-prod
+    cd ../../server
     sudo npm install
-    sudo node install --seed-data
-    sudo forever start app.js
+    sudo node install --seed-data 
+    sudo forever start app/app.js
 }
 
 puppet() {
-        sudo git clone https://github.com/RLOpenCatalyst/puppet-cookbook /opt/rlcatalyst/server/puppet-cookbook
+    sudo git clone https://github.com/RLOpenCatalyst/puppet-cookbook /opt/rlcatalyst/server/puppet-cookbook
 }
 
 vmware() {
-        cd ~
-        sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-        sudo curl -sSL https://get.rvm.io | bash
-        sudo /usr/local/rvm/bin/rvm install 2.1.0
-	sudo unlink /usr/bin/ruby
-        sudo ln -s  /usr/local/rvm/rubies/ruby-2.1.0/bin/ruby /usr/bin/ruby
-        sudo git clone https://github.com/RLOpenCatalyst/vmware /opt/vmware
-        cd /opt/vmware
-        sudo /usr/local/rvm/rubies/ruby-2.1.0/bin/gem install bundler
-        sudo /opt/vmware/bin/bundle
-	sudo ln -s /usr/local/rvm/rubies/ruby-2.1.0/bin/rails /usr/bin/rails
-        sudo chmod +x startup.sh
-        sudo /bin/bash startup.sh > /dev/null 2&>1
+     cd /opt
+     sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+     sudo curl -sSL https://get.rvm.io | bash
+     sudo /usr/local/rvm/bin/rvm install 2.1.4
+     sudo unlink /usr/bin/ruby
+     sudo ln -s  /usr/local/rvm/rubies/ruby-2.1.4/bin/ruby /usr/bin/ruby
+     sudo git clone https://github.com/RLOpenCatalyst/vmware /opt/vmware
+     cd /opt/vmware
+     sudo /usr/local/rvm/rubies/ruby-2.1.4/bin/gem install bundler
+     sudo /opt/vmware/bin/bundle
+     sudo chmod +x startup.sh
+     /bin/bash startup.sh
+     sudo ln -s /usr/local/rvm/rubies/ruby-2.1.4/bin/gem /usr/bin/gem
+     sudo gem install sass
+     sudo ln -s /usr/local/rvm/rubies/ruby-2.1.4/bin/sass /usr/bin/sass
+     sudo sed -i 's/ruby_executable_hooks/ruby/g' /usr/bin/sass
+     cd -
+}
+
+nodejs() {
+	cd /opt
+        sudo wget https://nodejs.org/dist/v4.4.4/node-v4.4.4-linux-x64.tar.gz
+        sudo tar zxvf node-v4.4.4-linux-x64.tar.gz
+        sudo mv node-v4.4.4-linux-x64 node
+        sudo ln -s /opt/node/bin/node /usr/bin/node
+        sudo ln -s /opt/node/bin/npm /usr/bin/npm
+        sudo npm install -g npm@3.5.2
+        sudo npm install -g forever
+        sudo npm install -g kerberos
+        sudo npm install -g grunt-cli
+        sudo ln -s /opt/node/bin/forever /usr/bin/forever
+        sudo ln -s /opt/node/bin/grunt /usr/bin/grunt
 }
 
 if [ "$OS" == "ubuntu" ] || [ "$OS" == "debian" ]
@@ -70,17 +92,7 @@ then
         sudo apt-get install -y g++ make libkrb5-dev curl git
 
         #Install the Nodejs
-        cd /opt
-        wget https://nodejs.org/dist/v4.2.2/node-v4.2.2-linux-x64.tar.gz
-        sudo tar zxvf node-v4.2.2-linux-x64.tar.gz
-        sudo mv node-v4.2.2-linux-x64 node
-        sudo ln -s /opt/node/bin/node /usr/bin/node
-        sudo ln -s /opt/node/bin/npm /usr/bin/npm
-        sudo npm install -g npm@3.4.0
-        sudo npm install -g forever
-	sudo ln -s /opt/node/bin/forever /usr/bin/forever
-        sudo npm install -g kerberos
-
+        nodejs
 
         #install chef-client
         chef1=$(dpkg-query -l | grep chef | awk '{print $2}'|head -1)
@@ -89,13 +101,14 @@ then
                 echo "Chef-Client has been installed"
         else
                 sudo curl -L https://www.opscode.com/chef/install.sh | sudo bash
+                sudo /opt/chef/embedded/bin/gem install knife-windows
         fi
 
 
         #Deploy Catalyst
+        vmware
         catalystdeploy
         puppet
-        vmware
         seeddataloader
         echo "Installation of Catalyst has been Completed. please login to http://<vagrantip>/<hostip>:vagrantport/hostport"
 fi
